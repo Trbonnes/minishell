cdc1ab16e39c94684ad9a34e72666b48bc306b46
Tristan BONNESON <trbonnes@e2r4p14.42.fr>
2020-01-22 12:16:43 +0100

    new file

---
+++ srcs/quote.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/22 12:08:45 by trbonnes          #+#    #+#             */
+/*   Updated: 2020/01/22 12:12:11 by trbonnes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/fonction.h"
+
+void	ft_unquote(char **str)
+{}
+
+int		ft_wait_quote(char **str, char c)
+{
+	char	*quote;
+	char	*join;
+	int		stop;
+	int		i;
+
+	stop = 0;
+	while (stop != 1)
+	{
+		i = -1;
+		write(1, ">", 1);
+		get_next_line(0, &quote);
+		while (quote[++i])
+			if (quote[i] == c)
+				stop = 1;
+		join = ft_strjoin(str[0], quote);
+		printf("free1\n");
+		free(quote);
+		printf("free2\n");
+		free(str[0]);
+		str[0] = ft_strdup(join);
+		printf("free3\n");
+		free(join);
+	}
+	i = 0;
+	join = ft_strdup_chr(str[0], c);
+	printf("free4\n");
+	free(str[0]);
+	str[0] = ft_strdup(join);
+	printf("free5\n");
+	free(join);
+	return (ft_strlen(str[0]));
+}
+
+int		ft_redirection_calculate(int i, char **str)
+{
+	while (str[0][i] && (str[0][i] == '<' || str[0][i] == '>' || str[0][i] == ' '))
+		i++;
+	if (str[0][i] == 34)
+	{
+		i++;
+		printf("double quote\n");
+		while (str[0][i] && str[0][i] != 34)
+			i++;
+		if (str[0][i] == '\0')
+			i = ft_wait_quote(str, 34);
+		else
+			i++;
+	}
+	else if (str[0][i] == 39)
+	{
+		i++;
+		printf("simple quote\n");
+		while (str[0][i] && str[0][i] != 39)
+			i++;
+		if (str[0][i] == '\0')
+			i = ft_wait_quote(str, 39);
+		else
+			i++;
+		
+	}
+	else
+		while (str[0][i] && str[0][i] != '<' && str[0][i] != '>' && str[0][i] != ' ')
+			i++;
+	return (i);
+}
--- srcs/stdin.c
+++ srcs/stdin.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 10:37:02 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/01/22 11:48:44 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/01/22 12:09:27 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,69 +71,6 @@ char	*ft_parser_cmd(char *str)
 		parsed[j++] = str[i++];
 	parsed[j] = '\0';
 	return (parsed);
-}
-
-int		ft_wait_quote(char **str, char c)
-{
-	char	*quote;
-	char	*join;
-	int		stop;
-	int		i;
-
-	stop = 0;
-	while (stop != 1)
-	{
-		i = -1;
-		write(1, ">", 1);
-		get_next_line(0, &quote);
-		while (quote[++i])
-			if (quote[i] == c)
-				stop = 1;
-		join = ft_strjoin(str[0], quote);
-		printf("free1\n");
-		free(quote);
-		printf("free2\n");
-		free(str[0]);
-		str[0] = ft_strdup(join);
-		printf("free3\n");
-		free(join);
-	}
-	i = 0;
-	join = ft_strdup_chr(str[0], c);
-	printf("free4\n");
-	free(str[0]);
-	str[0] = ft_strdup(join);
-	printf("free5\n");
-	free(join);
-	return (ft_strlen(str[0]));
-}
-
-int		ft_redirection_calculate(int i, char **str)
-{
-	while (str[0][i] && (str[0][i] == '<' || str[0][i] == '>' || str[0][i] == ' '))
-		i++;
-	if (str[0][i] == 34)
-	{
-		i++;
-		printf("double quote\n");
-		while (str[0][i] && str[0][i] != 34)
-			i++;
-		if (str[0][i] == '\0')
-			i = ft_wait_quote(str, 34);
-	}
-	else if (str[0][i] == 39)
-	{
-		i++;
-		printf("simple quote\n");
-		while (str[0][i] && str[0][i] != 39)
-			i++;
-		if (str[0][i] == '\0')
-			i = ft_wait_quote(str, 39);
-	}
-	else
-		while (str[0][i] && str[0][i] != '<' && str[0][i] != '>' && str[0][i] != ' ')
-			i++;
-	return (i);
 }
 
 int		ft_redirection_setup(char *str, char **actual)
@@ -203,6 +140,8 @@ char	*ft_parser_redirection(char **builtin_str)
 			return (NULL);
 		redirection_str[0] = '\0';
 	}
+	ft_unquote(&redirection_str);
+	ft_unquote(builtin_str);
 	return (redirection_str);
 }
 
