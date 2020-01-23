e55a16738c00145cce80595b248a7dd662235ef5
Tristan BONNESON <trbonnes@e1r10p9.42.fr>
2020-01-23 13:48:14 +0100

    initialisation option

--- srcs/stdin.c
+++ srcs/stdin.c
@@ -6,7 +6,7 @@
 /*   By: trbonnes <trbonnes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/17 10:37:02 by trbonnes          #+#    #+#             */
-/*   Updated: 2020/01/23 13:44:41 by trbonnes         ###   ########.fr       */
+/*   Updated: 2020/01/23 13:48:10 by trbonnes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -117,6 +117,7 @@ int		ft_detect_builtin(void)
 		{
 			if(!(parser->next = malloc(sizeof(t_parsing))))
 				return (-1);
+			parser->echo_option = 0;
 			parser = parser->next;
 			i++;
 		}
