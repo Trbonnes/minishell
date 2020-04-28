23e049e3ff3351492617baf7f15b23d61b10a09b
trbonnes <trbonnes@student.42.fr>
2020-04-28 14:58:30 +0200

    optimizing multiexport

--- srcs/fonction3.c
+++ srcs/fonction3.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 22:54:48 by trostan           #+#    #+#             */
-/*   Updated: 2020/04/28 14:28:08 by user42           ###   ########.fr       */
+/*   Updated: 2020/04/28 14:56:21 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,21 @@ extern t_env *g_env_list;
 void	ft_export_multi(t_parsing *alk)
 {
 	int		i;
-	int		j;
-	char	*exp;
+	char	**split;
 
 	i = 0;
-	j = 0;
-	while (alk->param[i])
+	split = check_param_env(alk);
+	while (split[i])
 	{
-		while (alk->param[i] != '=' && alk->param[i])
-			i++;
-		while (alk->param[i] != '\0' && alk->param[i] != ' ')
-			i++;
-		exp = ft_substr(alk->param, j, i - j);
-		ft_unquote(&exp, 2);
-		ft_same_export(exp);
-		if (alk->param[0] != '\0')
-			ft_lstadd_back(&g_env_list, ft_new_env(exp));
-		while (alk->param[i] != '\0' && alk->param[i] == ' ')
-			i++;
-		j = i;
-		free(exp);
+		ft_same_export(split[i]);
+		if (split[i][0] != '\0')
+			ft_lstadd_back(&g_env_list, ft_new_env(split[i]));
+		i++;
 	}
+	i = 0;
+	while (split[i])
+		free(split[i++]);
+	free(split);
 }
 
 void	ft_unset_multiple(char *uns)
